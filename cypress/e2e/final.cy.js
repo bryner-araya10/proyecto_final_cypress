c1380dab67fbd708bdfbf4dd30b4137a7bd69e18
Bryner Araya <baraya@cmc.local>
2023-07-06 19:58:38 -0600

    avance_060723

--- cypress/e2e/final.cy.js
+++ cypress/e2e/final.cy.js
@@ -1,5 +1,32 @@
-describe('Proyecto Final', () => {
-  it('Login', () => {
+describe('Proyecto final', () => {
+  beforeEach (() => {
     cy.visit('https://www.saucedemo.com/')
   })
-})
+  
+  // USUARIO INVALIDO
+    it('Login', () => {
+    cy.get('#user-name').type('problem_user');
+    cy.get('#password').type('secret_saucse');
+    cy.get('#login-button').click();
+    cy.get('.error-message-container').should('be.visible');
+  });
+
+    // USUARIO EN BLANCO
+    it('Login2', () => {
+    cy.get('#user-name').type('problem_user');
+    cy.get('#password').type(' ');
+    cy.get('#login-button').click();
+    cy.get('.error-message-container').should('be.visible');
+  });
+
+
+    // USUARIO CORRECTO
+    it('Login3', () => {
+    cy.get('#user-name').type('standard_user');
+    cy.get('#password').type('secret_sauce');
+    cy.get('#login-button').click();
+    cy.get("span").eq(0).contains("Products")
+  });
+
+});
+
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,7 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+Cypress.Commands.add("getLogin", (selector) => {
+    return cy.get(`[data-test=${selector}]`)
+  })
